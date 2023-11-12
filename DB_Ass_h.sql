51080ff1dca9c18d6880771facd95c09d1b51428
Li Hui <christine@christinedembp.mynetworksettings.com>
2023-11-12 07:19:20 -0500

    DB_PROJECT_P3

---
+++ DB_Ass_h.sql
@@ -0,0 +1,38 @@
+-- Count records in the OFFICE table
+SELECT COUNT(*) FROM OFFICE;
+
+-- Count records in the VCLASS table
+SELECT COUNT(*) FROM VCLASS;
+
+-- Count records in the VEHICLE table
+SELECT COUNT(*) FROM VEHICLE;
+
+-- Count records in the RENTAL_SERVICE table
+SELECT COUNT(*) FROM RENTAL_SERVICE;
+
+-- Count records in the CUSTOMER table
+SELECT COUNT(*) FROM CUSTOMER;
+
+-- Count records in the CORP_CUST table
+SELECT COUNT(*) FROM CORP_CUST;
+
+-- Count records in the INDIV_CUST table
+SELECT COUNT(*) FROM INDIV_CUST;
+
+-- Count records in the INVOICE table
+SELECT COUNT(*) FROM INVOICE;
+
+-- Count records in the INV_DISC table
+SELECT COUNT(*) FROM INV_DISC;
+
+-- Count records in the DISCOUNT table
+SELECT COUNT(*) FROM DISCOUNT;
+
+-- Count records in the CORP_DISC table
+SELECT COUNT(*) FROM CORP_DISC;
+
+-- Count records in the INDIVIDUAL_DISC table
+SELECT COUNT(*) FROM INDIVIDUAL_DISC;
+
+-- Count records in the PAYMENT table
+SELECT COUNT(*) FROM PAYMENT;
